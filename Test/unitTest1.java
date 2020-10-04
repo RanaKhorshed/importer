36cb8b6cea3782c761632ae5c7a708be2797526c
RanaKhorshed <71960643+RanaKhorshed@users.noreply.github.com>
2020-10-04 21:52:56 +0200

    nothing
    cs 3 lab 2

---
+++ Test/unitTest1.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class unitTest1 {
+    @Test
+    public int add(int x,int y){
+
+        return x+y;
+    }
+    public static int subtraction(int x,int y){
+        return x-y;
+    }
+    public static int multiplication(int x, int y){
+        return x*y;
+    }
+    public static int division(int x,int y){
+        return x/y;
+    }
+}
+
